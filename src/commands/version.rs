0af91f49d44867023a7fd5040ede4ecc5b13fc54
Fairy_Phy <phy.public@gmail.com>
2022-06-06 22:43:33 +0900

    add version command

--- src/commands/version.rs
+++ src/commands/version.rs
@@ -1,5 +1,38 @@
+use log::error;
 use serenity::builder::CreateApplicationCommandOption;
-use serenity::model::interactions::application_command::ApplicationCommandOptionType;
+use serenity::client::Context;
+use serenity::model::interactions::application_command::{ApplicationCommandInteraction, ApplicationCommandOptionType};
+use serenity::model::interactions::InteractionResponseType;
+use crate::utils::color;
+
+const PROJECT_NAME: &'static str = env!("CARGO_PKG_NAME");
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+fn get_project_name() -> String {
+	let mut p_name_vec: Vec<char> = PROJECT_NAME.chars().collect();
+	p_name_vec[0] = p_name_vec[0].to_ascii_uppercase();
+	return p_name_vec.iter().collect();
+}
+
+pub async fn execute(ctx: Context, command: ApplicationCommandInteraction) {
+	if let Err(error) = command.create_interaction_response(&ctx.http,
+		|res|
+			res
+				.kind(InteractionResponseType::ChannelMessageWithSource)
+				.interaction_response_data(|m| {
+					m
+						.create_embed(|e| {
+							e
+								.title("バージョン情報")
+								.description(format!("{} {}", get_project_name(), VERSION))
+								.color(color::normal_color())
+						})
+				})
+	).await {
+		error!("{}", error);
+		return;
+	}
+}
 
 pub fn command_build(option: &mut CreateApplicationCommandOption) -> &mut CreateApplicationCommandOption {
 	option
