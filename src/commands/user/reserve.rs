fd8c104f46b360c4d96c38e7eaf2dce9f19fd850
Fairy_Phy <phy.public@gmail.com>
2022-06-06 22:47:06 +0900

    add post vote from channel log at reserve command

--- src/commands/user/reserve.rs
+++ src/commands/user/reserve.rs
@@ -1,14 +1,17 @@
 use std::borrow::BorrowMut;
 use std::num::ParseIntError;
-use chrono::{DateTime, Duration, Utc};
+use chrono::{DateTime, Duration, Local, Utc};
 use log::{error, info};
 use serenity::builder::{CreateApplicationCommandOption, CreateComponents, CreateEmbed};
 use serenity::client::Context;
+use serenity::model::id::ChannelId;
 use serenity::model::interactions::application_command::{ApplicationCommandInteractionDataOption, ApplicationCommandInteraction, ApplicationCommandOptionType};
 use serenity::model::interactions::InteractionResponseType;
 use serenity::model::interactions::message_component::ButtonStyle;
+use sqlx::Error;
 use crate::STATIC_COMPONENTS;
 use crate::tables::{account, quaryfn};
+use crate::tables::guild::Config;
 use crate::utils::color;
 
 const PARAM_USERID: &str = "user_id";
@@ -109,6 +112,7 @@ pub async fn execute(ctx: Context, command: &ApplicationCommandInteraction, comm
 								.description("以下の内容で登録します")
 								.field("ユーザーID", &user_id, true)
 								.field("名前", &name, true)
+								.color(color::normal_color())
 						})
 						.components(|com| {
 							com.create_action_row(|ar| {
@@ -142,43 +146,105 @@ pub async fn execute(ctx: Context, command: &ApplicationCommandInteraction, comm
 			error!("{}", error);
 			return;
 		}*/
-
-		let pending_data = account::Pending {
-			uid: user_id,
-			name: name.clone(),
-			end_voting: Utc::now() + Duration::days(7)
-		};
+		if let Err(error) = button_interaction.defer(&ctx.http).await {
+			error!("{}", error);
+			return;
+		}
+		if let Err(error) = button_interaction.edit_original_interaction_response(&ctx.http, |res| {
+			res
+				.components(|c| {
+					c.set_action_rows(vec![])
+				})
+				.set_embeds(vec![])
+				.create_embed(|e| {
+					e
+						.title("処理中...")
+						.description("このメッセージを削除しないでください")
+						.color(color::normal_color())
+				})
+		}).await {
+			error!("{}", error);
+			return;
+		}
 
 		let mut lsc = STATIC_COMPONENTS.lock().await;
 		let mut locked_db = lsc.get_sql();
-		if let Err(error) = quaryfn::insert_pending_account(&pending_data, &locked_db).await {
-			error!("DB Error: {:?}", error);
-			error_message = Some(format!("{:?}", error));
+		if command.guild_id.is_none() {
+			error!("Should not occur error: GuildID is none");
+			return;
+		}
+		let guild_config = match quaryfn::get_guild_config(*command.guild_id.unwrap().as_u64(), &locked_db).await {
+			Ok(x) => Some(x),
+			Err(e) => {
+				error!("DB Error: {:?}", e);
+				error_message = Some(format!("{:?}", e));
+				None
+			}
+		};
+		if let Some(guild_config) = guild_config {
+			if let Some(guild_log_channel) = guild_config.log_channel_id {
+				let pending_data = account::Pending {
+					uid: user_id,
+					name: name.clone(),
+					message_id: 0,
+					end_voting: Utc::now() + Duration::days(7)
+				};
+
+				let log_channel = ChannelId(guild_log_channel);
+				if let Err(error) = log_channel.send_message(&ctx.http, |m| {
+					m.add_embed(|e| {
+						e
+							.title("追加申請")
+							.description("以下の内容で登録申請されました。これはテストです。")
+							.field("ユーザーID", &pending_data.uid, true)
+							.field("名前", &pending_data.name, true)
+							.field("申請却下終了時刻", &pending_data.end_voting.with_timezone(&Local).format("%Y/%m/%d %H:%M:%S"), true)
+							.color(color::normal_color());
+						if let Some(reason) = reason {
+							e
+								.field("申請理由", reason, true);
+						}
+						e
+					})
+				}).await {
+					error!("Error: {:?}", error);
+					return;
+				}
+
+				if let Err(error) = quaryfn::insert_pending_account(&pending_data, &locked_db).await {
+					error!("DB Error: {:?}", error);
+					error_message = Some(format!("{:?}", error));
+				}
+			}
+			else {
+				error!("Error: Not found log channel");
+				error_message = Some(String::from("ログチャンネルが指定されていません"));
+			}
 		}
 
-		if let Err(error) = button_interaction.create_interaction_response(&ctx.http, |res| {
+		std::mem::drop(lsc);
+
+		if let Err(error) = button_interaction.edit_original_interaction_response(&ctx.http, |res| {
 			res
-				.kind(InteractionResponseType::UpdateMessage)
-				.interaction_response_data(|m| {
-					m
-						.set_components(CreateComponents::default())
-						.embeds([])
-						.create_embed(|e| {
-							if let Some(err_msg) = error_message {
-								e
-									.title("エラー")
-									.description(err_msg)
-									.color(color::failed_color())
-							}
-							else {
-								e
-									.title("完了")
-									.description("以下の内容で登録しました！7日間何も無ければ正式に登録されます")
-									.field("ユーザーID", &user_id, true)
-									.field("名前", &name, true)
-									.color(color::success_color())
-							}
-						})
+				.components(|c| {
+					c.set_action_rows(vec![])
+				})
+				.set_embeds(vec![])
+				.create_embed(|e| {
+					if let Some(err_msg) = error_message {
+						e
+							.title("エラー")
+							.description(err_msg)
+							.color(color::failed_color())
+					}
+					else {
+						e
+							.title("完了")
+							.description("以下の内容で登録しました！7日間何も無ければ正式に登録されます")
+							.field("ユーザーID", &user_id, true)
+							.field("名前", &name, true)
+							.color(color::success_color())
+					}
 				})
 		}).await {
 			error!("{}", error);
