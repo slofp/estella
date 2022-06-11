5c5f4c9c8ac4431b1baf01ab798363825d41616d
Fairy_Phy <phy.public@gmail.com>
2022-06-11 19:27:23 +0900

    add and change query functions

--- src/tables/quaryfn.rs
+++ src/tables/quaryfn.rs
@@ -3,32 +3,36 @@ use crate::tables::{account, guild};
 
 /// select func
 
-pub async fn get_main_account(uid: u64, client: &Pool<MySql>) -> Result<account::Main, Error> {
-	sqlx::query_as::<_, account::Main>("select * from main_account where uid = ?")
+pub async fn get_main_account(guild_id: u64, uid: u64, client: &Pool<MySql>) -> Result<account::Main, Error> {
+	sqlx::query_as::<_, account::Main>("select * from main_account where guild_id = ? and uid = ?")
+		.bind(guild_id)
 		.bind(uid)
 		.fetch_one(client).await
 }
 
-pub async fn get_sub_account(uid: u64, client: &Pool<MySql>) -> Result<account::Sub, Error> {
-	sqlx::query_as::<_, account::Sub>("select * from sub_account where uid = ?")
+pub async fn get_sub_account(guild_id: u64, uid: u64, client: &Pool<MySql>) -> Result<account::Sub, Error> {
+	sqlx::query_as::<_, account::Sub>("select * from sub_account where guild_id = ? and uid = ?")
+		.bind(guild_id)
 		.bind(uid)
 		.fetch_one(client).await
 }
 
-pub async fn get_confirmed_account(uid: u64, client: &Pool<MySql>) -> Result<account::Confirmed, Error> {
-	sqlx::query_as::<_, account::Confirmed>("select * from confirmed_account where uid = ?")
+pub async fn get_confirmed_account(guild_id: u64, uid: u64, client: &Pool<MySql>) -> Result<account::Confirmed, Error> {
+	sqlx::query_as::<_, account::Confirmed>("select * from confirmed_account where guild_id = ? and uid = ?")
+		.bind(guild_id)
 		.bind(uid)
 		.fetch_one(client).await
 }
 
-pub async fn get_pending_account(uid: u64, client: &Pool<MySql>) -> Result<account::Pending, Error> {
-	sqlx::query_as::<_, account::Pending>("select * from pending_account where uid = ?")
+pub async fn get_pending_account(guild_id: u64, uid: u64, client: &Pool<MySql>) -> Result<account::Pending, Error> {
+	sqlx::query_as::<_, account::Pending>("select * from pending_account where guild_id = ? and uid = ?")
+		.bind(guild_id)
 		.bind(uid)
 		.fetch_one(client).await
 }
 
-pub async fn get_all_pending_account(client: &Pool<MySql>) -> Result<Vec<account::Pending>, Error> {
-	sqlx::query_as::<_, account::Pending>("select * from pending_account")
+pub async fn get_all_main_pending_account(client: &Pool<MySql>) -> Result<Vec<account::Pending>, Error> {
+	sqlx::query_as::<_, account::Pending>("select * from pending_account where account_type = 1")
 		.fetch_all(client).await
 }
 
@@ -38,12 +42,19 @@ pub async fn get_guild_config(uid: u64, client: &Pool<MySql>) -> Result<guild::C
 		.fetch_one(client).await
 }
 
+pub async fn get_main_sub_account(main_id: u64, client: &Pool<MySql>) -> Result<Vec<account::Sub>, Error> {
+	sqlx::query_as::<_, account::Sub>("select * from sub_account where main_uid = ?")
+		.bind(main_id)
+		.fetch_all(client).await
+}
+
 /// insert func
 
 pub async fn insert_main_account(value: &account::Main, client: &Pool<MySql>) -> Result<(), Error> {
-	sqlx::query("insert into main_account values (?, ?, ?, ?, default, default)")
+	sqlx::query("insert into main_account values (?, ?, ?, ?, ?, default, default)")
 		.bind(&value.uid)
 		.bind(&value.name)
+		.bind(&value.guild_id)
 		.bind(&value.version)
 		.bind(&value.join_date)
 		.execute(client).await?;
@@ -52,9 +63,10 @@ pub async fn insert_main_account(value: &account::Main, client: &Pool<MySql>) ->
 }
 
 pub async fn insert_sub_account(value: &account::Sub, client: &Pool<MySql>) -> Result<(), Error> {
-	sqlx::query("insert into sub_account values (?, ?, ?, ?, ?, ?)")
+	sqlx::query("insert into sub_account values (?, ?, ?, ?, ?, ?, ?)")
 		.bind(&value.uid)
 		.bind(&value.name)
+		.bind(&value.guild_id)
 		.bind(&value.join_date)
 		.bind(&value.main_uid)
 		.bind(&value.first_cert)
@@ -65,10 +77,27 @@ pub async fn insert_sub_account(value: &account::Sub, client: &Pool<MySql>) -> R
 }
 
 pub async fn insert_confirmed_account(value: &account::Confirmed, client: &Pool<MySql>) -> Result<(), Error> {
-	sqlx::query("insert into confirmed_account values (?, ?, ?, ?, ?)")
+	sqlx::query("insert into confirmed_account values (?, ?, ?, ?, ?, ?, ?)")
 		.bind(&value.uid)
 		.bind(&value.name)
+		.bind(&value.guild_id)
 		.bind(&value.account_type)
+		.bind(&value.main_uid)
+		.bind(&value.first_cert)
+		.bind(&value.second_cert)
+		.execute(client).await?;
+
+	Ok(())
+}
+
+pub async fn insert_pending_account(value: &account::Pending, client: &Pool<MySql>) -> Result<(), Error> {
+	sqlx::query("insert into pending_account values (?, ?, ?, ?, ?, ?, ?, ?)")
+		.bind(&value.uid)
+		.bind(&value.name)
+		.bind(&value.guild_id)
+		.bind(&value.account_type)
+		.bind(&value.message_id)
+		.bind(&value.end_voting)
 		.bind(&value.main_uid)
 		.bind(&value.first_cert)
 		.execute(client).await?;
@@ -76,12 +105,18 @@ pub async fn insert_confirmed_account(value: &account::Confirmed, client: &Pool<
 	Ok(())
 }
 
-pub async fn insert_pending_account(value: &account::Pending, client: &Pool<MySql>) -> Result<(), Error> {
-	sqlx::query("insert into pending_account values (?, ?, ?, ?)")
-		.bind(&value.uid)
-		.bind(&value.name)
-		.bind(&value.message_id)
-		.bind(&value.end_voting)
+pub async fn init_user_data(uid: u64, glacialeur: Option<String>, client: &Pool<MySql>) -> Result<(), Error> {
+	sqlx::query("insert into user_data values (?, ?)")
+		.bind(uid)
+		.bind(glacialeur)
+		.execute(client).await?;
+
+	Ok(())
+}
+
+pub async fn init_user_level(uid: u64, client: &Pool<MySql>) -> Result<(), Error> {
+	sqlx::query("insert into pending_account values (?)")
+		.bind(uid)
 		.execute(client).await?;
 
 	Ok(())
@@ -90,9 +125,55 @@ pub async fn insert_pending_account(value: &account::Pending, client: &Pool<MySq
 /// update func
 
 pub async fn update_main_account(value: &account::Main, client: &Pool<MySql>) -> Result<(), Error> {
-	sqlx::query("update main_account set is_leaved = ? where uid = ?")
+	sqlx::query("update main_account set is_leaved = ? where guild_id = ? and uid = ?")
 		.bind(&value.is_leaved)
+		.bind(&value.guild_id)
 		.bind(&value.uid)
+		.execute(client).await?;
+
+	Ok(())
+}
+
+pub async fn update_guild_config_log(guild_id: u64, id: u64, client: &Pool<MySql>) -> Result<(), Error> {
+	sqlx::query("update guild_config set log_channel_id = ? where uid = ?")
+		.bind(id)
+		.bind(guild_id)
+		.execute(client).await?;
+
+	Ok(())
+}
+
+pub async fn update_guild_config_auth(guild_id: u64, id: u64, client: &Pool<MySql>) -> Result<(), Error> {
+	sqlx::query("update guild_config set auth_role_id = ? where uid = ?")
+		.bind(id)
+		.bind(guild_id)
+		.execute(client).await?;
+
+	Ok(())
+}
+
+pub async fn update_guild_config_bot(guild_id: u64, id: u64, client: &Pool<MySql>) -> Result<(), Error> {
+	sqlx::query("update guild_config set bot_role_id = ? where uid = ?")
+		.bind(id)
+		.bind(guild_id)
+		.execute(client).await?;
+
+	Ok(())
+}
+
+pub async fn update_guild_config_white(guild_id: u64, value: bool, client: &Pool<MySql>) -> Result<(), Error> {
+	sqlx::query("update guild_config set white_list = ? where uid = ?")
+		.bind(value)
+		.bind(guild_id)
+		.execute(client).await?;
+
+	Ok(())
+}
+
+pub async fn update_guild_config_leave(guild_id: u64, value: bool, client: &Pool<MySql>) -> Result<(), Error> {
+	sqlx::query("update guild_config set leave_ban = ? where uid = ?")
+		.bind(value)
+		.bind(guild_id)
 		.execute(client).await?;
 
 	Ok(())
@@ -125,21 +206,21 @@ pub async fn update_main_account(value: &account::Main, client: &Pool<MySql>) ->
 	Ok(())
 }*/
 
-// 今の所使用しない
-/*pub async fn update_pending_account(value: &account::Pending, client: &Pool<MySql>) -> Result<(), Error> {
-	sqlx::query("update pending_account set ")
+pub async fn update_pending_sub_account(value: &account::Pending, client: &Pool<MySql>) -> Result<(), Error> {
+	sqlx::query("update pending_account set first_cert = ? where guild_id = ? and uid = ?")
+		.bind(&value.first_cert)
+		.bind(&value.guild_id)
 		.bind(&value.uid)
-		.bind(&value.name)
-		.bind(&value.end_voting)
-		.fetch_one(client).await?;
+		.execute(client).await?;
 
 	Ok(())
-}*/
+}
 
 /// delete func
 
 pub async fn delete_main_account(value: &account::Main, client: &Pool<MySql>) -> Result<(), Error> {
-	sqlx::query("delete from main_account where uid = ?")
+	sqlx::query("delete from main_account where guild_id = ? and uid = ?")
+		.bind(&value.guild_id)
 		.bind(&value.uid)
 		.execute(client).await?;
 
@@ -147,7 +228,8 @@ pub async fn delete_main_account(value: &account::Main, client: &Pool<MySql>) ->
 }
 
 pub async fn delete_sub_account(value: &account::Sub, client: &Pool<MySql>) -> Result<(), Error> {
-	sqlx::query("delete from sub_account where uid = ?")
+	sqlx::query("delete from sub_account where guild_id = ? and uid = ?")
+		.bind(&value.guild_id)
 		.bind(&value.uid)
 		.execute(client).await?;
 
@@ -155,7 +237,8 @@ pub async fn delete_sub_account(value: &account::Sub, client: &Pool<MySql>) -> R
 }
 
 pub async fn delete_confirmed_account(value: &account::Confirmed, client: &Pool<MySql>) -> Result<(), Error> {
-	sqlx::query("delete from confirmed_account where uid = ?")
+	sqlx::query("delete from confirmed_account where guild_id = ? and uid = ?")
+		.bind(&value.guild_id)
 		.bind(&value.uid)
 		.execute(client).await?;
 
@@ -163,7 +246,8 @@ pub async fn delete_confirmed_account(value: &account::Confirmed, client: &Pool<
 }
 
 pub async fn delete_pending_account(value: &account::Pending, client: &Pool<MySql>) -> Result<(), Error> {
-	sqlx::query("delete from pending_account where uid = ?")
+	sqlx::query("delete from pending_account where guild_id = ? and uid = ?")
+		.bind(&value.guild_id)
 		.bind(&value.uid)
 		.execute(client).await?;
 
@@ -175,9 +259,10 @@ pub async fn delete_pending_account(value: &account::Pending, client: &Pool<MySq
 /// bot admin control func
 
 pub async fn insert_main_account_manual(value: &account::Main, client: &Pool<MySql>) -> Result<(), Error> {
-	sqlx::query("insert into main_account values (?, ?, ?, ?, ?, ?)")
+	sqlx::query("insert into main_account values (?, ?, ?, ?, ?, ?, ?)")
 		.bind(&value.uid)
 		.bind(&value.name)
+		.bind(&value.guild_id)
 		.bind(&value.version)
 		.bind(&value.join_date)
 		.bind(&value.is_sc)
